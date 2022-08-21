8fb1336691859e668ae02a906afd90ebe8501acb
iryna-romashchenko <irromas@ukr.net>
2022-08-21 13:46:31 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const imagesContainer = document.querySelector(".gallery");
+const imageCard = onCreateGalleryItem(galleryItems);
+
+imagesContainer.insertAdjacentHTML("beforeend", imageCard);
+
+function onCreateGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class = "gallery__item" href="${original}">
+    <img class ="gallery__image lazyload"
+    data-src = "${preview}"
+    alt = "${description}"/>
+    </a>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  disableRightClick: true,
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: "alt",
+});
