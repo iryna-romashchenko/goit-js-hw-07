0fe3c909aa485a24d286ae79b1af64f13df17b27
iryna-romashchenko <irromas@ukr.net>
2022-08-21 11:46:36 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const imagesContainer = document.querySelector(".gallery");
+const imageCard = onCreateGalleryItem(galleryItems);
+let openOriginalImage;
+
+imagesContainer.insertAdjacentHTML("beforeend", imageCard);
+imagesContainer.addEventListener("keydown", onEscToCloseImage);
+
+function onCreateGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <div class ="gallery__item">
+            <a class ="gallery__link" href='${original}'>
+            <img
+            class ="gallery__image lazyload"
+
+            data-src = "${preview}"
+            data-source = "${original}"
+            alt = "${description}"
+            />
+            </a>
+            </div>
+        `;
+    })
+    .join("");
+}
+imagesContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  openOriginalImage = basicLightbox.create(`
+    <img src = "${event.target.dataset.source}" width="1280" height ="900">`);
+  openOriginalImage.show();
+});
+function onEscToCloseImage(event) {
+  if (event.code !== "Escape") {
+    return;
+  }
+  openOriginalImage.close();
+}
